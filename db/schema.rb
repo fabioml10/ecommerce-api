666bca2d79e3ce9f9ff20836ef66ea072e5cc98b
Fabio Muller <fabioml10@gmail.com>
2021-12-23 23:40:48 -0300

    Add featured on products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_24_014805) do
+ActiveRecord::Schema.define(version: 2021_12_24_023401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 2021_12_24_014805) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "status"
+    t.boolean "featured", default: false
     t.index ["productable_type", "productable_id"], name: "index_products_on_productable"
   end
 
