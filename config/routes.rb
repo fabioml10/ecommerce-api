0e0b27e6a431b16bbf4f00ca2cf9e5629e00ced2
Fabio Muller <fabioml10@gmail.com>
2021-12-28 17:10:14 -0300

    Add product search service

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   namespace :admin, defaults: { format: :json } do
     namespace :v1 do
+      get "home" => "home#index"
       resources :categories
       resources :coupons
       resources :system_requirements
