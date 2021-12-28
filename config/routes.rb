af3e4dafc6ffd01df02950ae7264abd9333520d2
Fabio Muller <fabioml10@gmail.com>
2021-12-28 17:43:01 -0300

    Add coupon validation endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
       resources :categories, only: :index
       resources :products, only: [:index, :show]
       resources :wish_items, only: [:index, :create, :destroy]
+      post "/coupons/:coupon_code/validations", to: "coupon_validations#create"
     end
   end
 end
