65e23d42893c121390a920c8a1638039fd3c3ccb
Fabio Muller <fabioml10@gmail.com>
2021-12-28 17:40:18 -0300

    Add coupn validation

--- spec/factories/coupons.rb
+++ spec/factories/coupons.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :coupon do
     sequence(:name) { |n| "My Coupon #{n}" }
     code { Faker::Commerce.unique.promotion_code(digits: 6) }
-    status { %i(active inactive).sample }
+    status { :active }
     discount_value { rand(1..99) }
     max_use { rand(1..99) }
     due_date { rand(1..30).days.from_now }
