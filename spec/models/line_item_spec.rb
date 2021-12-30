a5d038147ff334bd32477a52dbc6536433f507a4
Fabio Muller <fabioml10@gmail.com>
2021-12-30 11:01:53 -0300

    Add status to line items

--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe LineItem, type: :model do
   it { is_expected.to validate_numericality_of(:quantity).only_integer.is_greater_than(0) }
   it { is_expected.to validate_presence_of :payed_price }
   it { is_expected.to validate_numericality_of(:payed_price).is_greater_than(0) }
+  it { is_expected.to validate_presence_of(:status).on(:update) }
+  it { is_expected.to define_enum_for(:status).with_values(waiting_order: 1, preparing: 2, en_route: 3, delivered: 4) }
 
   it { is_expected.to belong_to :order }
   it { is_expected.to belong_to :product }
@@ -15,5 +17,10 @@ RSpec.describe LineItem, type: :model do
     subject = build(:line_item, payed_price: payed_price, quantity: quantity)
     expected_value = payed_price * quantity
     expect(subject.total).to eq expected_value
+  end
+
+  it "receives :waiting_order status as default on creation" do
+    subject = create(:line_item, status: nil)
+    expect(subject.status).to eq 'waiting_order'
   end
 end
