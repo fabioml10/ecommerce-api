d16634f5aa216ba8011755ff37d311bcecd78f35
Fabio Muller <fabioml10@gmail.com>
2021-12-30 13:31:48 -0300

    Add authentication on sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # Configure Sidekiq-specific session middleware
   Sidekiq::Web.use ActionDispatch::Cookies
   Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: "_interslice_session"
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    username == ENV["SIDEKIQ_USERNAME"] && password == ENV["SIDEKIQ_PASSWORD"]
+  end
   EcommerceApi::Application.routes.draw do
     mount Sidekiq::Web => "/sidekiq"
   end
